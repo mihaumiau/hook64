47ef8f9f6c1b3ee2e5a644e2a507997acfe88e2e
mihaumiau <hannichen00@gmail.com>
2026-09-16 12:46:25 +0200

    Update hook64.h

--- hook64.h
+++ hook64.h
@@ -1,13 +1,14 @@
 #pragma once
 
 typedef enum {
-    HOOK_SUCCEED,
     HOOK_INVALID_MOD_NAME,
     HOOK_UNKNOWN_MOD,
     HOOK_NO_CAVE_FOUND,
     HOOK_INVALID_FUN_NAME,
     HOOK_INVALID_WRITEBACK,
-    HOOK_UNKNOWN_FUN
+    HOOK_UNKNOWN_FUN,
+    HOOK_RELOAD_FAILED,
+    HOOK_SUCCEED,
 } hook_status;
 
 typedef struct {
@@ -16,4 +17,12 @@ typedef struct {
     void** orginal;
 } hook_detour_entry;
 
-hook_status hook_module(unsigned short* module_name, hook_detour_entry detours[], int detour_count);
+hook_status hook_module(char* module_name, hook_detour_entry detours[], int detour_count);
+
+typedef enum {
+    HOOK_RELOAD_FAILED_SNAPSHOT,
+    HOOK_RELOAD_NO_MODULES,
+    HOOK_RELOAD_SUCCEED,
+} hook_reload_status;
+
+hook_reload_status hook_reload(char* module_name);
