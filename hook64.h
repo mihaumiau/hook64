e44b6de6fa290beb26af1fe7a4374616b59203b0
mihaumiau <hannichen00@gmail.com>
2026-09-17 13:02:19 +0200

    Update hook64.h

--- hook64.h
+++ hook64.h
@@ -19,9 +19,9 @@ typedef struct {
 hook_status hook_module(char* module_name, hook_detour_entry detours[], int detour_count);
 
 typedef enum {
+    HOOK_RELOAD_UNKNOWN_MOD,
     HOOK_RELOAD_FAILED_SNAPSHOT,
     HOOK_RELOAD_NO_MODS,
-    HOOK_RELOAD_UNKNOWN_MOD,
     HOOK_RELOAD_SUCCEED,
 } hook_reload_status;
 
